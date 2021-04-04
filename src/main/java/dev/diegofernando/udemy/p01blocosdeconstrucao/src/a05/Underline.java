7dcd07a58eeab6fe50ee5b3ddf48e145050da039
diegofsousa <diegofernando5672@gmail.com>
2021-04-04 16:47:48 -0300

    Conteúdo tipo referência vs. tipo primitivo

---
+++ src/main/java/dev/diegofernando/udemy/p01blocosdeconstrucao/src/a05/Underline.java
@@ -0,0 +1,10 @@
+package dev.diegofernando.udemy.p01blocosdeconstrucao.src.a05;
+
+public class Underline {
+    public static void main(String[] args) {
+        int million = 1_000_000;
+        int million2 = 1_000__000;
+        System.out.println(million);
+        System.out.println(million2);
+    }
+}
